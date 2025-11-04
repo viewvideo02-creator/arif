fbe7886ebc32ab42cd7e989d7139c1f2ecf01a1d
arif <viewvideo02@gmail.com>
2025-11-04 13:23:45 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,99 @@
+import requests
+import random
+import time
+import os
+from colorama import Fore
+
+print("   ____          ____       _                    ")
+print("  | __ )  __ _  |  _ \ __ _| |_ ___ _ __   __ _  ")
+print("  |  _ \ / _' | | |_) / _' | __/ _ \ '_ \ / _' | ")
+print("  | |_) | (_| | |  __/ (_| | ||  __/ | | | (_| | ")
+print("  |____/ \__, | |_|   \__,_|\__\___|_| |_|\__, | ")
+print("         |___/                            |___/  \n")
+print("=================================================")
+author = "Bg.Pateng"
+print("Author: " + author)
+script = "Push Rank Discord"
+print("Script: " + script)
+telegram = "@bangpateng_group"
+print("Telegram: " + telegram)
+youtube = "Bang Pateng"
+print("Youtube: " + youtube)
+print("===========================================")
+print('PERINGATAN : TIDAK UNTUK DI PERJUAL-BELIKAN')
+print("===========================================\n")
+
+time.sleep(1)
+
+import json
+
+# baca config otomatis
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+channel_id = config["channel_id"]
+waktu1 = config["delay_delete"]
+waktu2 = config["delay_send"]
+
+# tampilkan info rapi
+print("[INFO] Setting Bot:")
+print(f"  Channel ID   : {channel_id}")
+print(f"  Hapus tiap   : {waktu1} detik")
+print(f"  Kirim tiap   : {waktu2} detik")
+
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+with open("pesan.txt", "r") as f:
+    words = f.readlines()
+
+with open("token.txt", "r") as f:
+    authorization = f.readline().strip()
+
+i = 0
+while True:
+    channel_id = channel_id.strip()
+
+    payload = {
+        'content': words[i].strip()
+    }
+
+    headers = {
+        'Authorization': authorization
+    }
+
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+    print(Fore.WHITE + "Sent message: ")
+    print(Fore.YELLOW + payload['content'])
+
+    response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+
+    if response.status_code == 200:
+        messages = response.json()
+        if len(messages) == 0:
+            break
+        else:
+            time.sleep(waktu1)
+
+            message_id = messages[0]['id']
+            response = requests.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}', headers=headers)
+            if response.status_code == 204:
+                print(Fore.GREEN + f'Pesan dengan ID {message_id} berhasil dihapus')
+            else:
+                print(Fore.RED + f'Gagal menghapus pesan dengan ID {message_id}: {response.status_code}')
+    else:
+        print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
+
+    # Lanjut ke pesan berikutnya
+    i += 1
+    if i >= len(words):  # kalau sudah sampai akhir file
+        i = 0  # balik lagi ke awal
+
+    time.sleep(waktu2)
